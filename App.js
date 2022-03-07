900c24ab450992369bc0812dce2856a92424c257
luan-antonio <46446054+luan-antonio@users.noreply.github.com>
2022-03-07 15:36:39 -0300

    feat(dotenv): config env
    Configure env as soon as possible

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const mongoose = require("mongoose");
 
 const { DB_USER, DB_PASS, PORT } = process.env;
